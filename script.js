3fb4bb727f59f0e0d3dcf1d1a1fd1279718bcb38
VL.dilankarw <dilankarashmika10@gmail.com>
2024-03-30 00:04:41 +0530

    Update

--- script.js
+++ script.js
@@ -23,74 +23,7 @@
                 sidemenu.style.right = "-200px";
             }
 
-    //         <!-- ---------------- Script for Submit Data & Google Sheet ----------------
-    // <script>
-    //     const scriptURL = 'Your Google sheet > Web App URL'
-    //     const form = document.forms['submit-to-google-sheet']
-    //     const msg = document.getElementById("msg")
-    
-    //     form.addEventListener('submit', e => {
-    //     e.preventDefault()
-    //     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    //         .then(response => {
-    //             msg.innerHTML = "Message set successfully"
-    //             setTimeout(function(){
-    //                 msg.innerHTML = ""
-    //             },5000)
-    //             // message will remove after 5000ms (5s).
-    //             form.reset()
-    //         })
-    //         .catch(error => console.error('Error!', error.message))
-    //     })
-    // </script> -->
-
-
-
-    // <!-- This Code For Google Sheet > Extensions > Apps Script > Code.gs -->
-
-    // <!-- var sheetName = 'Sheet1'
-    // var scriptProp = PropertiesService.getScriptProperties()
-    
-    // function intialSetup () {
-    //   var activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
-    //   scriptProp.setProperty('key', activeSpreadsheet.getId())
-    // }
-    
-    // function doPost (e) {
-    //   var lock = LockService.getScriptLock()
-    //   lock.tryLock(10000)
-    
-    //   try {
-    //     var doc = SpreadsheetApp.openById(scriptProp.getProperty('key'))
-    //     var sheet = doc.getSheetByName(sheetName)
-    
-    //     var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
-    //     var nextRow = sheet.getLastRow() + 1
-    
-    //     var newRow = headers.map(function(header) {
-    //       return header === 'timestamp' ? new Date() : e.parameter[header]
-    //     })
-    
-    //     sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow])
-    
-    //     return ContentService
-    //       .createTextOutput(JSON.stringify({ 'result': 'success', 'row': nextRow }))
-    //       .setMimeType(ContentService.MimeType.JSON)
-    //   }
-    
-    //   catch (e) {
-    //     return ContentService
-    //       .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
-    //       .setMimeType(ContentService.MimeType.JSON)
-    //   }
-    
-    //   finally {
-    //     lock.releaseLock()
-    //   }
-    // } -->
-
-
-    // <!-- ------------------------------------------------------------------------------------------ -->
+// <!-- ------------------------------------------------------------------------------------------ -->
 
         const wrapper = document.querySelector(".wrapper");
         const carousel = document.querySelector(".carousel");
