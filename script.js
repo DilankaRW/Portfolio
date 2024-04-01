302c14c7e517e9298386384a205bb4d535edb280
VL.dilankarw <dilankarashmika10@gmail.com>
2024-04-02 00:02:01 +0530

    Update

--- script.js
+++ script.js
@@ -23,24 +23,25 @@
                 sidemenu.style.right = "-200px";
             }
 
-            // // ---------------- Script for Submit Data & Google Sheet ----------------
-            // const scriptURL = 'Your Google sheet > Web App URL'
-            // const form = document.forms['submit-to-google-sheet']
-            // const msg = document.getElementById("msg")
+// ---------------- Script for Submit Data & Google Sheet ----------------
+
+        // const scriptURL = 'Your Google sheet > Web App URL'
+        // const form = document.forms['submit-to-google-sheet']
+        // const msg = document.getElementById("msg")
         
-            // form.addEventListener('submit', e => {
-            // e.preventDefault()
-            // fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            //     .then(response => {
-            //         msg.innerHTML = "Message set successfully"
-            //         setTimeout(function(){
-            //             msg.innerHTML = ""
-            //         },5000)
-            //         // message will remove after 5000ms (5s).
-            //         form.reset()
-            //     })
-            //     .catch(error => console.error('Error!', error.message))
-            // })
+        // form.addEventListener('submit', e => {
+        // e.preventDefault()
+        // fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        //     .then(response => {
+        //         msg.innerHTML = "Message set successfully"
+        //         setTimeout(function(){
+        //             msg.innerHTML = ""
+        //         },5000)
+        //         // message will remove after 5000ms (5s).
+        //         form.reset()
+        //     })
+        //     .catch(error => console.error('Error!', error.message))
+        // })
 
 
 // <!-- ------------------------------------------------------------------------------------------ -->
