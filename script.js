1d8ed3fc6d99198275cb4d4c0c5ff4861caa6520
VL.dilankarw <dilankarashmika10@gmail.com>
2024-04-01 23:58:25 +0530

    Update

--- script.js
+++ script.js
@@ -23,6 +23,26 @@
                 sidemenu.style.right = "-200px";
             }
 
+            // // ---------------- Script for Submit Data & Google Sheet ----------------
+            // const scriptURL = 'Your Google sheet > Web App URL'
+            // const form = document.forms['submit-to-google-sheet']
+            // const msg = document.getElementById("msg")
+        
+            // form.addEventListener('submit', e => {
+            // e.preventDefault()
+            // fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            //     .then(response => {
+            //         msg.innerHTML = "Message set successfully"
+            //         setTimeout(function(){
+            //             msg.innerHTML = ""
+            //         },5000)
+            //         // message will remove after 5000ms (5s).
+            //         form.reset()
+            //     })
+            //     .catch(error => console.error('Error!', error.message))
+            // })
+
+
 // <!-- ------------------------------------------------------------------------------------------ -->
 
         const wrapper = document.querySelector(".wrapper");
